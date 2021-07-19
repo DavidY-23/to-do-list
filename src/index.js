baa6376fb430a72722ca0071d3990aa65fb408c8
unknown <davidye0201@gmail.com>
2021-07-18 21:40:50 -0400

    Got the delete button to work

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import './style.css';
 
+/***
+ * All the adding project stuff.
+ * 
+ */
+
+//Creates the sidebar. 
 const sidebar = document.getElementById("sidenav");
 const sideProject = document.getElementById("projects")
-var projects = ['Projects'];
-console.log(sidebar);
-console.log(document.getElementById("content"));
-console.log(sideProject);
 
-sidebar.innerHTML += projects[0];
+//Delete Button.
+const DeleteButton = document.createElement("button");
+DeleteButton.innerHTML = "X";
+
+//Adds a new project.
 function AddProject(){
     let newProjectItem = document.createElement('div');
     newProjectItem.classList.add('project');
@@ -16,12 +22,38 @@ function AddProject(){
         return;
     }
     else {
-        //newProjectItem.setAttribute(newProject);
         newProjectItem.textContent = newProject;
         newProjectItem.id = newProject;
         sidebar.appendChild(newProjectItem);
-    }
+        newProjectItem.appendChild(DeleteButton);
 
+    }
 }
+
+//Button to delete a project.
+function DeleteProject() {
+    this.parentNode.remove()
+}
+
+DeleteButton.addEventListener("click", DeleteProject);
+
 sidebar.innerHTML =`To Do List <button id="newProject">Add</button><br>`;
+ 
+//Initial project.
+let firstProject = document.createElement('div');
+firstProject.classList.add('project');
+firstProject.id= "firstProject";
+firstProject.textContent = "Project";
+firstProject.appendChild(DeleteButton);
+sidebar.appendChild(firstProject);
+
 document.getElementById("newProject").addEventListener("click", AddProject);
+
+
+
+
+
+/***
+ * Adding tasks.
+ */
+
