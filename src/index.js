de7a914d009b551e8f3fd545b0c0ba541dbfc776
unknown <davidye0201@gmail.com>
2021-07-19 13:58:15 -0400

    Adding tasks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './style.css';
 
 /***
  * All the adding project stuff.
- * 
  */
 
 //Creates the sidebar. 
@@ -14,46 +13,83 @@ const DeleteButton = document.createElement("button");
 DeleteButton.id = "deleteButton";
 DeleteButton.innerHTML = "X";
 
+//Adding a new task.
+function newTask() {
+    let newTask = prompt("Enter your new task", "");
+    if (newTask == null || newTask == "") {
+        return;
+    }
+    let newTaskIdea = document.createElement('div');
+    let newDeleteButton = document.createElement("button");
+    let br = document.createElement("br");
+
+    newDeleteButton.id = "deleteButton";
+    newDeleteButton.innerHTML = "X";
+
+    newDeleteButton.addEventListener("click", DeleteProject);
+    //newTaskIdea.addEventListener("click", taskView);
+    newTaskIdea.classList.add('task');
+    newTaskIdea.textContent = newTask;
+    newTaskIdea.id = newTask;
+    document.getElementById("tasks").append(br);
+    document.getElementById("tasks").append(newTask);
+    //newTask.appendChild(newDeleteButton);
+
+}   
+
+//To change between Projects.
+function taskView() {
+    const theTasks = document.getElementById("tasks");
+    let newTaskButton = document.createElement("button");
+    newTaskButton.textContent = "New Task";
+    newTaskButton.addEventListener("click", newTask);
+    theTasks.innerHTML = this.id;
+    theTasks.appendChild(newTaskButton);
+}
+
 //Adds a new project.
 function AddProject(){
+    let newProject = prompt("Enter your new project", "");
+    if (newProject == null || newProject == "") {
+        return;
+    }
     let newProjectItem = document.createElement('div');
     let newDeleteButton = document.createElement("button");
     newDeleteButton.id = "deleteButton";
     newDeleteButton.innerHTML = "X";
     newDeleteButton.addEventListener("click", DeleteProject);
+    newProjectItem.addEventListener("click", taskView);
     newProjectItem.classList.add('project');
-    let newProject = prompt("Enter your new project", "");
-    if (newProject == null || newProject == "") {
-        return;
-    }
-    else {
-        newProjectItem.textContent = newProject;
-        newProjectItem.id = newProject;
-        sidebar.appendChild(newProjectItem);
-        newProjectItem.appendChild(newDeleteButton);
-        console.log(newDeleteButton.id);
-
-    }
+    newProjectItem.textContent = newProject;
+    newProjectItem.id = newProject;
+    sidebar.appendChild(newProjectItem);
+    newProjectItem.appendChild(newDeleteButton);
 }
 
-//Button to delete a project.
+//Button to delete project.
 function DeleteProject() {
-    this.parentNode.remove()
-}
+    this.parentNode.remove();
+    console.log(this);
+    console.log("Parent" + this.parentNode);
 
+}
 
 sidebar.innerHTML =`To Do List <button id="newProject">Add</button><br>`;
  
 //Initial project.
 let firstProject = document.createElement('div');
 firstProject.classList.add('project');
-firstProject.id= "firstProject";
+firstProject.id= "Project";
 firstProject.textContent = "Project";
+firstProject.addEventListener("click", taskView);
 firstProject.appendChild(DeleteButton);
 sidebar.appendChild(firstProject);
 
-document.getElementById("newProject").addEventListener("click", AddProject);
+
 document.getElementById("deleteButton").addEventListener("click", DeleteProject);
+document.getElementById("newProject").addEventListener("click", AddProject);
+
+
 
 
 
